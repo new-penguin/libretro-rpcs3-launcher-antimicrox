2f2c177c502440c256e00233ae39c3cb61ccedce
Tony <139792946+new-penguin@users.noreply.github.com>
2024-01-11 20:15:16 -0600

    Update and rename libretro-yuzu-launcher.c to libretro-cemu-launcher.c

--- libretro-cemu-launcher.c
+++ libretro-cemu-launcher.c
@@ -43,10 +43,10 @@ void retro_set_controller_port_device(unsigned port, unsigned device)
 void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
-   info->library_name     = "Yuzu Launcher";
+   info->library_name     = "Cemu Launcher";
    info->library_version  = "1.0";
    info->need_fullpath    = true;
-   info->valid_extensions = "bin|iso|img|xci|nsp|nca|nso|nro|dump|chd";
+   info->valid_extensions = "bin|iso|img|wud|wux|rpx|dump|chd";
 }
 
 static retro_video_refresh_t video_cb;
@@ -121,7 +121,7 @@ void retro_reset(void)
 /**
  * libretro callback; Called every game tick.
  *
- * Once the core has run, we will attempt to exit, since yuzu is done.
+ * Once the core has run, we will attempt to exit, since cemu is done.
  */
 void retro_run(void)
 {
@@ -129,7 +129,7 @@ void retro_run(void)
    unsigned stride = 320;
    video_cb(frame_buf, 320, 240, stride << 2);
 
-   // Shutdown the environment now that yuzu has loaded and quit.
+   // Shutdown the environment now that cemu has loaded and quit.
    environ_cb(RETRO_ENVIRONMENT_SHUTDOWN, NULL);
 }
 
@@ -138,45 +138,45 @@ void retro_run(void)
  */
 bool retro_load_game(const struct retro_game_info *info)
 {
-   // Launch without the gui if available (yuzu).
-   char command[512] = "antimicrox & yuzu -f -g";
+   // Launch without the gui if available (cemu).
+   char command[512] = "antimicrox & cemu -f -g";
 
    // Check if there is content to load.
    if (info != NULL && info->path != NULL && info->path[0] != '\0') {
       sprintf(command, "%s \"%s\"", command, info->path);
    }
 
-   // Check if running yuzu works.
+   // Check if running cemu works.
    if (system(command) == 0) {
-      printf("libretro-yuzu-launcher: Completed Yuzu\n");
+      printf("libretro-cemu-launcher: Completed Cemu\n");
       return true;
    }
 
    // Flatpak
-   printf("libretro-yuzu-launcher: Yuzu not found. Attempting Flatpak...\n");
-   strcpy(command, "flatpak run io.github.antimicrox.antimicrox & flatpak run org.yuzu_emu.yuzu -f -g");
+   printf("libretro-cemu-launcher: Cemu not found. Attempting Flatpak...\n");
+   strcpy(command, "flatpak run io.github.antimicrox.antimicrox & flatpak run info.cemu.Cemu -f -g");
    if (info != NULL && info->path != NULL && info->path[0] != '\0') {
       // Execute with --batch.
       sprintf(command, "%s \"%s\"", command, info->path);
    }
    if (system(command) == 0) {
-      printf("libretro-yuzu-launcher: Finished running Yuzu through Flatpak.\n");
+      printf("libretro-cemu-launcher: Finished running Cemu through Flatpak.\n");
       return true;
    }
 
    // AppImage
-   printf("libretro-yuzu-launcher: Yuzu not found. Attempting AppImage...\n");
-   strcpy(command, "~/.config/retroarch/system/antimicrox.AppImage & ~/.config/retroarch/system/yuzu.AppImage -f -g");
+   printf("libretro-cemu-launcher: Cemu not found. Attempting AppImage...\n");
+   strcpy(command, "~/.config/retroarch/system/antimicrox.AppImage & ~/.config/retroarch/system/cemu.AppImage -f -g");
    if (info != NULL && info->path != NULL && info->path[0] != '\0') {
       // Execute with --batch.
       sprintf(command, "%s \"%s\"", command, info->path);
    }
    if (system(command) == 0) {
-      printf("libretro-yuzu-launcher: Finished running Yuzu through AppImage.\n");
+      printf("libretro-cemu-launcher: Finished running Cemu through AppImage.\n");
       return true;
    }
 
-   printf("libretro-yuzu-launcher: Failed running Yuzu. Install it and try again.\n");
+   printf("libretro-cemu-launcher: Failed running Cemu. Install it and try again.\n");
    return false;
 }
 
